523119b33e5c8e8f31adf791f8147bee121d7174
cks1031 <cks1031@naver.com>
2024-08-26 14:07:12 +0900

    GalleryService에 갤러리 관련 게시글과 첨부파일 정보 저장 기능 구현

--- app/service/gallery.py
+++ app/service/gallery.py
@@ -1,6 +1,10 @@
 import os
 
 from fastapi import Form
+from sqlalchemy import insert
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.model.gallery import Gallery, GalAttach
 from app.schema.gallery import NewGallery
 
 UPLOAD_PATH = 'C:/Java/nginx-1.26.2/html/cdn/img/'
@@ -27,4 +31,26 @@ async def process_upload(files):
     return attachs
 
 class GalleryService:
-    pass
+    @staticmethod
+    def insert_gallery(gal, attachs, db):
+        try:
+            stmt = insert(Gallery).values(userid=gal.userid,
+                                         title=gal.title, contents=gal.contents)
+            result = db.execute(stmt)
+
+            # 방금 생성한 레코드의 기본키 값 : inserted_primary_key
+            inserted_gno = result.inserted_primary_key[0]
+
+            for attach in attachs:
+                data = {'fname': attach[0], 'fsize': attach[1],
+                        'gno': inserted_gno}
+                stmt = insert(GalAttach).values(data)
+                result = db.execute(stmt)
+
+            db.commit()
+
+            return result
+
+        except SQLAlchemyError as ex:
+            print(f'▶▶▶ insert_gallery에서 오류발생 : {str(ex)}')
+            db.rollback()
