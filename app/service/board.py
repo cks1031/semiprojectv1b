0a273f1d9b7f8e360af5a209d7dda4f31aef11cb
cks1031 <cks1031@naver.com>
2024-08-29 13:41:03 +0900

    게시글 본문보기시 댓글출력 기능 구현 수정

--- app/service/board.py
+++ app/service/board.py
@@ -56,14 +56,15 @@ class BoardService:
             db.execute(stmt)
 
             # 본문글 + 댓글 읽어오기
-            stmt = select(Board).join(Board.replys)\
+            # outerjoin : outer join
+            # contains_eager : 관계 맺은 하위 객체의 내용을 즉시 로딩
+            stmt = select(Board).outerjoin(Board.replys)\
                     .options(contains_eager(Board.replys))\
-                    .where(Board.bno == bno)\
-                    .order_by(Reply.rpno)
-            result = db.execute(stmt).scalars().first()
-
+                    .where(Board.bno == bno).order_by(Reply.rpno)
+            result = db.execute(stmt)
             db.commit()
-            return result
+
+            return result.scalars().first()
 
         except SQLAlchemyError as ex:
                 print(f'▶▶▶ select_board 오류발생 : {str(ex)}')
